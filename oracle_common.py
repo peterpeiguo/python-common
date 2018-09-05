1472e489c2de46c616231cb6c3593acc1ba18e39
Peter Guo <peterpeiguo@gmail.com>
2018-09-05 10:48:17 -0700

    added minotor func

--- oracle_common.py
+++ oracle_common.py
@@ -1,4 +1,15 @@
-def query(connection, query, condition): 
+'''
+pip install cx_Oracle
+document: http://cx-oracle.readthedocs.io/en/latest/installation.html
+
+tutorial:
+http://www.oracle.com/technetwork/articles/dsl/python-091105.html
+'''
+
+from time import *
+import cx_Oracle
+
+def query_database(connection, query, condition): 
     cursor = connection.cursor()
     cursor.prepare(query)
     cursor.execute(None, condition)
@@ -8,3 +19,18 @@ def query(connection, query, condition):
     cursor.close()
 
     return result
+
+def monitor(user, password, db, query, condition, interval):
+    known_keys = {}
+    while True:
+        connection = cx_Oracle.connect(user, password, db)
+        print(f"oracle version = {connection.version}")
+        rows = query_database(connection, query, condition)
+        connection.close()
+        print(strftime("%H:%M", localtime()))
+        for row in rows:
+            if row[0] not in known_keys:
+                print(row)
+                known_keys[row[0]] = True
+        sleep(interval)
+        print("=====================")        
