61a47014259c72a9d81a06039982be1806b3a709
Peter Guo <peterpeiguo@gmail.com>
2018-09-05 09:07:40 -0700

    use the prams passed in

--- oracle_common.py
+++ oracle_common.py
@@ -1,7 +1,7 @@
 def query(connection, query, condition): 
     cursor = connection.cursor()
     cursor.prepare(query)
-    cursor.execute(None, {})
+    cursor.execute(None, condition)
     result = []
     for row in cursor:
         result.append(row)
